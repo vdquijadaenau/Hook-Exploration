5848146a9bd8f221802a99f0264df7a6444f0d69
vdquijadaenau <vdquijadaenau@gmail.com>
2022-03-23 13:46:38 +1100

    test for Evenlistener

--- src/hooks/useEventListener/useEventListener.ts
+++ src/hooks/useEventListener/useEventListener.ts
@@ -1,47 +1,39 @@
-import {RefObject, useEffect, useRef} from 'react'
-import {useIsomorphicLayoutEffect} from '../useIsomorphicLayoutEffect'
+import { RefObject, useEffect, useRef } from 'react';
 
-function useEventListener<K extends keyof WindowEventMap>(
-eventName:K,
-handler:(event: WindowEventMap[K])=>void
-):void
+import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
 
-function useEventListener<
-K extends keyof HTMLElementEventMap,
-T extends HTMLElement = HTMLDivElement
->(
-    eventName: K,
-    handler:(Event:HTMLElementEventMap[K])=>void,
-    element:RefObject<T>
-):void
+function useEventListener<K extends keyof WindowEventMap>(eventName: K, handler: (event: WindowEventMap[K]) => void): void;
+function useEventListener<K extends keyof HTMLElementEventMap, T extends HTMLElement = HTMLDivElement>(
+  eventName: K,
+  handler: (event: HTMLElementEventMap[K]) => void,
+  element: RefObject<T>,
+): void;
 
-function useEventListener<KW extends keyof WindowEventMap,KH extends keyof HTMLElementEventMap,T extends HTMLElement | void =void,>
-(
-    eventName: KW|KH,
-    handler:( event: WindowEventMap[KW] | HTMLElementEventMap[KH]| Event) =>void,
-    element?:RefObject<T>,
-){
-    const savedHandler = useRef(handler)
+function useEventListener<KW extends keyof WindowEventMap, KH extends keyof HTMLElementEventMap, T extends HTMLElement | void = void>(
+  eventName: KW | KH,
+  handler: (event: WindowEventMap[KW] | HTMLElementEventMap[KH] | Event) => void,
+  element?: RefObject<T>,
+) {
+  const savedHandler = useRef(handler);
 
-    useIsomorphicLayoutEffect(() => {
-        savedHandler.current = handler
-    },[handler])
+  useIsomorphicLayoutEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
 
-    useEffect(() => {
-        const targetElement:T|Window =element?.current || window
-        if(!(targetElement && targetElement.addEventListener)){
-            return
-        }
-        const eventListener:typeof handler = event => savedHandler.current(event)
+  useEffect(() => {
+    const targetElement: T | Window = element?.current || window;
+    if (!(targetElement && targetElement.addEventListener)) {
+      return;
+    }
 
-        targetElement.addEventListener(eventName, eventListener)
+    const eventListener: typeof handler = event => savedHandler.current(event);
 
-        return () => {
-            targetElement.removeEventListener(eventName, eventListener)
-        }
+    targetElement.addEventListener(eventName, eventListener);
 
-    },[eventName, element])
-
+    return () => {
+      targetElement.removeEventListener(eventName, eventListener);
+    };
+  }, [eventName, element]);
 }
 
-export default useEventListener
+export default useEventListener;
